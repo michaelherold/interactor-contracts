8a04dcbefc608cfb57fec9fe9ce1018f4fb35543
Michael Herold <michael.j.herold@gmail.com>
2016-02-04 22:31:57 -0600

    Add assurances

--- lib/interactor/contracts.rb
+++ lib/interactor/contracts.rb
@@ -1,6 +1,7 @@
 require "dry-validation"
 require "interactor"
 
+# :nodoc:
 module Interactor
   # Create a contract for your interactor that specifies what it expects as
   # inputs.
@@ -17,6 +18,38 @@ module Interactor
       # @!attribute [r] expectations
       #   @return [Dry::Validations::Schema] the expectations schema
       attr_reader :expectations
+
+      # The assurances the Interactor will fulfill.
+      #
+      # @!attribute [r] assurances
+      #   @return [Dry::Validations::Schema] the assurances schema
+      attr_reader :assurances
+
+      # Defines the assurances of an Interactor and creates an after hook to
+      # validate the output when called.
+      #
+      # @example
+      #
+      #   class CreatePerson
+      #     include Interactor
+      #     include Interactor::Contracts
+      #
+      #     assures do
+      #       attr(:person, &:filled?)
+      #     end
+      #
+      #     def call
+      #       context.person = Person.new
+      #     end
+      #   end
+      # @param [Block] block the block defining the assurances
+      # @return [void]
+      def assures(&block)
+        @assurances ||= Class.new(Dry::Validation::Schema)
+        @assurances.instance_exec(&block)
+
+        define_after_hook
+      end
 
       # Defines the expectations of an Interactor and creates a before hook to
       # validate the input when called.
@@ -49,6 +82,22 @@ module Interactor
       end
 
       private
+
+      # Defines an after hook that validates the Interactor's output against
+      # its contract.
+      #
+      # @raise [Interactor::Failure] if the input fails to meet its contract.
+      # @return [void]
+      def define_after_hook
+        after do
+          assurances = self.class.assurances.new
+          result = assurances.call(context)
+
+          unless result.success?
+            context.fail!
+          end
+        end
+      end
 
       # Defines a before hook that validates the Interactor's input against its
       # contract.
--- spec/interactor/contracts_spec.rb
+++ spec/interactor/contracts_spec.rb
@@ -16,4 +16,26 @@ RSpec.describe Interactor::Contracts do
       expect(interactor.call(:first_name => "Billy")).to be_a_failure
     end
   end
+
+  describe ".promises" do
+    it "creates and uses a schema to validate outputs" do
+      interactor = Class.new do
+        include Interactor
+        include Interactor::Contracts
+
+        assures do
+          attr(:name, &:filled?)
+        end
+
+        def call
+          if context.enabled?
+            context.name = "Billy"
+          end
+        end
+      end
+
+      expect(interactor.call(:enabled? => true)).to be_a_success
+      expect(interactor.call(:enabled? => false)).to be_a_failure
+    end
+  end
 end
