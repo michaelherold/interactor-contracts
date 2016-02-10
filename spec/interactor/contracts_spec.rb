4f074b01c2a3f6ec03d74f8ac6932cf60efe1e15
Michael Herold <michael.j.herold@gmail.com>
2016-02-09 21:39:33 -0600

    Ensure that contracts can be made piecemeal

--- spec/interactor/contracts_spec.rb
+++ spec/interactor/contracts_spec.rb
@@ -21,6 +21,31 @@ RSpec.describe Interactor::Contracts do
       expect(interactor.call(:enabled? => true)).to be_a_success
       expect(interactor.call(:enabled? => false)).to be_a_failure
     end
+
+    it "can be called more than once" do
+      interactor = Class.new do
+        include Interactor
+        include Interactor::Contracts
+
+        assures do
+          attr(:first_name, &:filled?)
+        end
+
+        assures do
+          attr(:last_name, &:filled?)
+        end
+
+        def call
+          if context.enabled?
+            context.first_name = "Billy"
+            context.last_name  = "Boyd"
+          end
+        end
+      end
+
+      expect(interactor.call(:enabled? => true)).to be_a_success
+      expect(interactor.call(:enabled? => false)).to be_a_failure
+    end
   end
 
   describe ".expects" do
@@ -35,6 +60,29 @@ RSpec.describe Interactor::Contracts do
       end
 
       expect(interactor.call(:name => "Billy")).to be_a_success
+      expect(interactor.call(:first_name => "Billy")).to be_a_failure
+    end
+
+    it "can be called more than once" do
+      interactor = Class.new do
+        include Interactor
+        include Interactor::Contracts
+
+        expects do
+          attr(:first_name, &:filled?)
+        end
+
+        expects do
+          attr(:last_name, &:filled?)
+        end
+      end
+
+      expect(
+        interactor.call(
+          :first_name => "Billy",
+          :last_name => "Boyd"
+        )
+      ).to be_a_success
       expect(interactor.call(:first_name => "Billy")).to be_a_failure
     end
   end
