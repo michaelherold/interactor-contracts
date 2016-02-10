29707e22a64fe4548096b50c6c44886d3fa5fd9f
Michael Herold <michael.j.herold@gmail.com>
2016-02-09 21:34:39 -0600

    Fix the name of the assures method in a test

--- spec/interactor/contracts_spec.rb
+++ spec/interactor/contracts_spec.rb
@@ -1,23 +1,7 @@
 require "spec_helper"
 
 RSpec.describe Interactor::Contracts do
-  describe ".expects" do
-    it "creates and uses a schema to validate inputs" do
-      interactor = Class.new do
-        include Interactor
-        include Interactor::Contracts
-
-        expects do
-          attr(:name, &:filled?)
-        end
-      end
-
-      expect(interactor.call(:name => "Billy")).to be_a_success
-      expect(interactor.call(:first_name => "Billy")).to be_a_failure
-    end
-  end
-
-  describe ".promises" do
+  describe ".assures" do
     it "creates and uses a schema to validate outputs" do
       interactor = Class.new do
         include Interactor
@@ -36,6 +20,22 @@ RSpec.describe Interactor::Contracts do
 
       expect(interactor.call(:enabled? => true)).to be_a_success
       expect(interactor.call(:enabled? => false)).to be_a_failure
+    end
+  end
+
+  describe ".expects" do
+    it "creates and uses a schema to validate inputs" do
+      interactor = Class.new do
+        include Interactor
+        include Interactor::Contracts
+
+        expects do
+          attr(:name, &:filled?)
+        end
+      end
+
+      expect(interactor.call(:name => "Billy")).to be_a_success
+      expect(interactor.call(:first_name => "Billy")).to be_a_failure
     end
   end
 end
